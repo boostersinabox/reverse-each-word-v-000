b660d40c8fc43a6da11eaceacbd3d5332b27e4bb
Stacy Carr <stacy@boostersinabox.com>
2018-07-10 20:08:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,5 @@ end
 phrase = "Hello there, and how are you?"
 
 def reverse_each_word(phrase)
-  phrase.reverse.split.reverse.join(" ")
+  phrase.split.collect {|each_word| each_word.reverse}.join(" ")
 end  

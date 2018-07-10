37a58ce0218163cc02c54df51d7bc939e5a52632
Stacy Carr <stacy@boostersinabox.com>
2018-07-10 20:06:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,9 @@ def reverse_each_word(phrase)
   phrase.reverse.split.reverse.join(" ")
 end
   
+  
+phrase = "Hello there, and how are you?"
+
+def reverse_each_word(phrase)
+  phrase.reverse.split.reverse.join(" ")
+end  
